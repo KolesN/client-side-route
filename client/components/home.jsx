00af4dd0ba0ae373052c15d270ed29f46b1aaa13
root <root@DESKTOP-03C6QQU.localdomain>
2022-09-27 22:02:52 +0300

    fixed routes

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
+import dashMain from './dash-main'
+import dashProfile from './dash-profile'
+import dashboard from './dashboard'
 import Head from './head'
 // import wave from '../assets/images/wave.jpg'
 
@@ -19,6 +22,9 @@ const Home = () => {
         <div className="flex flex-col justify-center bg-neutral-900 p-10 rounded-xl select-none">
           <img alt="wave" src="images/logo-new-text.png" />
           <div id="title">Home</div>
+          <Route exact path="/dashboard" component={dashboard} />
+          <Route exact path="/dashboard/main" component={dashMain} />
+          <Route exact path="/dashboard/profile/:user" component={dashProfile} />
           <Link to="/dashboard">Go To Root</Link>
           <Link to="/dashboard/main">Go to Main </Link>
           <Link to="/dashboard/profile/e7aec42d-a611-47cc-a5c8-93e75ce35f1c">Go to Profile </Link>
